c23ba4c2f0c848a7db6003d38816699b5c08c7a9
Rishabh Prajapati <prajapatirishabh04@gmail.com>
2023-03-31 14:05:34 -0700

    Created custom user model and user manager

--- backend/Deliga/deliga/settings.py
+++ backend/Deliga/deliga/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'deliga',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# dev
+
+AUTH_USER_MODEL = 'account.User'
--- backend/Deliga/deliga/urls.py
+++ backend/Deliga/deliga/urls.py
@@ -18,5 +18,5 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/',include(('apps.account.urls','account'),namespace='account'))
+    path('api/account/',include(('apps.account.urls','account'),namespace='account'))
 ]
