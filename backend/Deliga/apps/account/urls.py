590cd91218cac361c6e0227f65069630fa0662bf
Rishabh Prajapati <prajapatirishabh04@gmail.com>
2023-04-01 00:26:32 -0700

    Created ChangePasswordAPI in account app

--- backend/Deliga/apps/account/urls.py
+++ backend/Deliga/apps/account/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path('register/',views.UserRegistrationView.as_view(),name='register_api'),
     path('login/',views.UserLoginView.as_view(),name='login_api'),
+    path('change-password/',views.UserChangePasswordView.as_view(),name='change-password'),
+    
 ]
--- backend/Deliga/apps/account/views.py
+++ backend/Deliga/apps/account/views.py
@@ -46,3 +46,14 @@ class UserLoginView(APIView):
             else:
                 return Response({'errors' : 'Invalid username or password!'})
         return Response(serializer.errors)
+
+class UserChangePasswordView(APIView):
+    permission_classes = [IsAuthenticated]
+    renderer_classes = [UserRenderer]
+    def post(self,request,format=None):
+        serializer = UserChangePasswordSerializer(data=request.data,context={
+            "user" : request.user
+        })
+        if serializer.is_valid():
+            return Response({'message': 'Password Change Successfully!!'},status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
